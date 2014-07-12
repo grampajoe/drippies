1521084474144424b52e18616869b794735ed8cd
Joe Friedl <joe@joefriedl.net>
2014-07-11 22:52:34 -0400

    Hack together some junk to try it out

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,21 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var $ = require('jquery');
+
+function showWeather(data) {
+  var place = data.results[0];
+
+  $('#location').val(place.formatted_address);
+}
  
 $('form').on('submit', function() {
+  var query = $('#location').val();
+
+  $.get(
+    'http://maps.googleapis.com/maps/api/geocode/json',
+    {'address': query},
+    showWeather
+  );
+
   return false;
 });
 
--- static/js/drippies.js
+++ static/js/drippies.js
@@ -1,5 +1,19 @@
 var $ = require('jquery');
+
+function showWeather(data) {
+  var place = data.results[0];
+
+  $('#location').val(place.formatted_address);
+}
  
 $('form').on('submit', function() {
+  var query = $('#location').val();
+
+  $.get(
+    'http://maps.googleapis.com/maps/api/geocode/json',
+    {'address': query},
+    showWeather
+  );
+
   return false;
 });
